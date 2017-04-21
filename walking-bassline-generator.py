eca83616ee09c6babd940bad2df6863e88385f7e
ericmschow <eric.m.schow@gmail.com>
2017-04-21 14:59:08 -0500

    prints major chord tones but only C major for some reason

--- walking-bassline-generator.py
+++ walking-bassline-generator.py
@@ -1,9 +1,11 @@
 # program intended to generate walking basslines using chord tones and chromatic
-# approach tones
+# approach tones.
+# outputs scale degrees relative to tonic, not root of each chord.
 
 # this is probably something that could be done better with classes
 # but I don't know how to use classes yet
 
+from random import randint
 
 # dict holding chord quality and tuple for semitones above root
 CHORDS = {
@@ -35,9 +37,25 @@ NOTES = {
     'B' : 12
 }
 
+# dict holding circle of fifths starting with 1 : C for calculating thirds
+FIFTHS = {
+    1 : 'C',
+    2 : 'G',
+    3 : 'D',
+    4 : 'A',
+    5 : 'E',
+    6 : 'B',
+    7 : 'Gb/F#',
+    8 : 'Db/C#',
+    9 : 'Ab/G#',
+   10 : 'Eb/D#',
+   11 : 'Bb/A#',
+   12 : 'F'
+}
+
 # prompts user for chords and returns string in format 'C Am F G'
 def chordPrompter():
-    chords = input("Please enter the chords, in format e.g. C Am F G\n> ")
+    chords = input("Please enter the chords, in format e.g. C Am F G:\n> ")
     return chords
 
 # prompts user for time signature and returns string in format '4/4'
@@ -55,7 +73,7 @@ def tempoPrompter():
 def chordParser(arg):
     chords = []
     chords = arg.split(' ')
-    print(chords)
+    # print(chords)
     return chords
 
 # parses user string for desired time signature and returns
@@ -69,32 +87,60 @@ def listUnpacker(arg):
     for chord in arg:
         root = rootFinder(chord)
         quality = qualityFinder(chord)
-        chordInfo = (root, quality)
+        chordInfo = (root, quality) # stores root and chord quality in tuple
         chordList.append(chordInfo)
     return chordList
 
 # reads list item and returns chord root as semitones
-def rootFinder(chord):
-    return NOTES[chord[0]]
+def rootFinder(chordListItem):
+    return NOTES[chordListItem[0].upper()]
 
 # reads list item and returns chord quality
-def qualityFinder(chord):
-    if 'maj7' in chord.lower():
+def qualityFinder(chordListItem):
+    if 'maj7' in chordListItem.lower():
         return 'maj7'
-    elif 'min7' in chord.lower():
+    elif 'min7' in chordListItem.lower():
         return 'min7'
-    elif 'dom7' in chord.lower():
+    elif 'dom7' in chordListItem.lower():
         return 'dom7'
-    elif 'm' in chord.lower():
+    elif 'm' in chordListItem.lower():
         return 'minor'
     else:
         return 'major'
 
+# returns random note in chord from tuple
+def randomizer(chordTuple):
+    pass
 
+# calls randomizer and returns
+def notePrinter(chordTuple):
+    timeSig = 4 # hardcode 4/4 until implementing variable time signature
+
+# returns tuple of chord tones given chord tuple checking dictionary
+def chordToneFinder(chordTuple):
+    quality = chordTuple[1]
+    tones = CHORDS[quality]
+    if quality == 'major':
+        for tone in tones:
+            root = int(tones[0])
+            third = root + 4
+            fifth = root + 1
+            return (FIFTHS[root], FIFTHS[third], FIFTHS[fifth])
+    if quality == 'minor':
+        pass
+    if quality == 'min7':
+        print("Extensions not yet implemented.")
+    # etc with other qualities later
 
 def main():
     chordList = chordParser(chordPrompter())
-    print(listUnpacker(chordList))
+    print("chordList is: ",chordList)                            ## DEBUG
+    tupleList = listUnpacker(chordList)
+    print("tupleList is: ",tupleList)                            ## DEBUG
+    for chord in tupleList:
+        print("Chord tuple is: ",chord)                            ## DEBUG
+        chordToneFinder(chord)
+        print(chordToneFinder(chord))
 
 
 
